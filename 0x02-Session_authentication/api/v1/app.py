65a4fdaca8f9854aa82d2e01573d596081ee0672
deenakamal <deenah.kamal@gmail.com>
2024-08-09 01:09:41 +0000

    edit

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,7 +56,8 @@ def before_request():
             or not auth.require_auth(request.path, paths_without_auth)):
         return
 
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None
+            and auth.session_cookie(request) is None):
         abort(401)
 
     if auth.current_user(request) is None:
